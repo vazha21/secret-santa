04f0021ee288f5b79793c3dc56e2ecff8de1731e
Gvrito123 <gvritishvili.gio123@gmail.com>
2020-11-27 11:20:47 +0400

    feat(finished getName, getMail, emailSend)

---
+++ script/script.js
@@ -0,0 +1,51 @@
+
+const getName = (randomName) => {
+    let arr = [
+        'Giorgi',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+        'saxeli',
+    ]
+    return arr[randomName];
+}
+
+const getMail = (name) => {
+    let arr = [
+        'gvritagio@gmail.com',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+        'maili',
+    ]
+    return arr[name];
+}
+
+const emailSend = (mail,randomName) => {
+    Email.send({
+        SecureToken : '5819ab7a-23cf-4a87-ad50-4f3d75a998ca',
+        To : mail,
+        From : 'cikvisanta2020@gmail.com',
+        Subject : 'Secret Santa',
+        Body : `You are santa of ${randomName}`
+    }).then(
+        message => alert(message))
+}
+
+// syntax : emailSend("mimgebis maili", "random saxeli")
+
+
+
+
+
+// Password : '6990C2BB6621EF250B1B8E64C80730256E92',
